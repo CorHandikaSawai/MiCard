5788764d79922f7138b23bc6ab579cd5f3bbabd5
Cor Sawai <handika033@gmail.com>
2021-08-06 18:04:34 -0500

    Finished

--- micard/lib/main.dart
+++ micard/lib/main.dart
@@ -11,10 +11,75 @@ class MiCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Color(0xFF212121),
         body: SafeArea(
-          child: Container(
-            child: Text('This is a test'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 100,
+                backgroundImage: AssetImage('images/cor.jpg'),
+              ),
+              Text(
+                'Cor Handika Sawai',
+                style: TextStyle(
+                    fontSize: 40,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'StyleScript'),
+              ),
+              Text(
+                'Flutter Developer',
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.white,
+                  fontFamily: 'SourceSansPro',
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Color(0xFF616161),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                color: Color(0xFF616161),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Color(0xFF212121),
+                  ),
+                  title: Text(
+                    '1234567890',
+                    style: TextStyle(
+                        fontFamily: 'SourceSansPro',
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 20),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                color: Color(0xFF616161),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Color(0xFF212121),
+                  ),
+                  title: Text(
+                    'myemail@email.com',
+                    style: TextStyle(
+                        fontFamily: 'SourceSansPro',
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 20),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
